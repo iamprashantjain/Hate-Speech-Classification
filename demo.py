4d6a6efa9fe889dd003c80137dfbf65829448a7c
iamprashant <iamprashantjain2601@gmail.com>
2025-02-14 00:21:48 +0530

    folder structure created

---
+++ demo.py
@@ -0,0 +1,10 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate-speech-2024","dataset.zip", "dataset/dataset.zip")
+
+
